9fe2499fc7d19129ff933984eb7c7e9b12a6a345
joonhyuck-hyoung <joonhyuck-hyoung@nhn-commerce.com>
2022-06-04 00:30:22 +0900

    JPA basic - START

---
+++ 1. JPA basic/README.md
@@ -0,0 +1,3 @@
+# 1. 자바 ORM 표준 JPA 프로그래밍 - 기본편
+
+### https://www.inflearn.com/course/ORM-JPA-Basic/dashboard
---
+++ 1. JPA basic/basic/src/test/java/study/jpa/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jpa.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
