7ae4673615da1773c6befab67ebffa5aa552cad8
gudwnsgur <gudwnsgur12@naver.com>
2022-09-06 16:44:13 +0900

    [10주차] í 프로젝트 환경설정 - 프로젝트 생성

---
+++ 2. JPA deep/jpashop/src/main/kotlin/jpabook/jpashop/JpashopApplication.kt
@@ -0,0 +1,11 @@
+package jpabook.jpashop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpashopApplication
+
+fun main(args: Array<String>) {
+	runApplication<JpashopApplication>(*args)
+}
