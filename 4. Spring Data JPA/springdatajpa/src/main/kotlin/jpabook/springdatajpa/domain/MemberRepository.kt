826cfb822ef1a7e308ba80415cbb59713c96513c
gudwnsgur <gudwnsgur12@naver.com>
2022-12-13 20:12:55 +0900

    [4.쿼리메서드] NamedQuery

--- 4. Spring Data JPA/springdatajpa/src/main/kotlin/jpabook/springdatajpa/domain/MemberRepository.kt
+++ 4. Spring Data JPA/springdatajpa/src/main/kotlin/jpabook/springdatajpa/domain/MemberRepository.kt
@@ -1,10 +1,14 @@
 package jpabook.springdatajpa.domain
 
 import org.springframework.data.jpa.repository.*
+import org.springframework.data.repository.query.*
 
 /**
  * @author Joonhyuck Hyoung
  */
 interface MemberRepository : JpaRepository<Member, Long> {
     fun findByUsernameAndAgeGreaterThan(username: String, age: Int): List<Member>
+
+    @Query(name = "Member.findByUsername") // 생략 가능
+    fun findByUsername(@Param("username") username: String): List<Member>
 }
