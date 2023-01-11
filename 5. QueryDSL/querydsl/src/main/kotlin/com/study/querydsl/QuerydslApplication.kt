10c4b571f86a1df4fb151ba8f2b0a93e20d7c06b
gudwnsgur <gudwnsgur12@naver.com>
2023-01-11 19:18:06 +0900

    [19주차 querydsl]  프로젝트 환경설정 - 프로젝트 생성

---
+++ 5. QueryDSL/querydsl/src/main/kotlin/com/study/querydsl/QuerydslApplication.kt
@@ -0,0 +1,11 @@
+package com.study.querydsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuerydslApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuerydslApplication>(*args)
+}
